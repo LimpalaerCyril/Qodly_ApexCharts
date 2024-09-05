f77470f72ec1c68a91abd86b3471d5bc2a86ef9a
Ayoub METWALLI <ayoub.metwalli@4d.com>
2024-09-05 11:07:51 +0100

    add bubble chart

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -4,12 +4,14 @@ import Pie from './Pie';
 import Area from './Area';
 import Bar from './Bar';
 import Line from './Line';
+import Bubble from './Bubble';
 
 export default {
-  // components
-  Donut,
-  Pie,
-  Area,
-  Bar,
-  Line,
+	// components
+	Donut,
+	Pie,
+	Area,
+	Bar,
+	Line,
+	Bubble,
 };
